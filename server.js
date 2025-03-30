130c61f44dfa2cd9cf40579e9a87d360fbe3ef89
mangesh <mangeshlemte.a23@gmail.com>
2025-03-30 15:54:17 +0530

    #

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cron = require("node-cron");
-const emailjs = require("@emailjs/nodejs");
+const emailjs = require("emailjs-com");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
